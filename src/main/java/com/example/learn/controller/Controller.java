2640c8c126708d0995243b70ef51e2567bf62a26
wyt <wyet12@163.com>
2021-12-06 23:03:43 +0800

    主分支修改

--- src/main/java/com/example/learn/controller/Controller.java
+++ src/main/java/com/example/learn/controller/Controller.java
@@ -1,4 +1,15 @@
 package com.example.learn.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class Controller {
+
+     @RequestMapping("/client1")
+     public String client1(){
+         return "success111221";
+     }
+
 }
