df54f3fb31fb8e5e315adbd76e3a000e89c9602b
wyt <wyet12@163.com>
2021-12-06 22:42:29 +0800

    新增文件

--- src/main/java/com/example/learn/controller/Controller.java
+++ src/main/java/com/example/learn/controller/Controller.java
@@ -1,4 +1,12 @@
 package com.example.learn.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class Controller {
+
+
+
 }
